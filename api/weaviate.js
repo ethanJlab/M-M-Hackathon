d21b36ff26c5fa80a2e81f90fa1ec280c59b6d9b
KevinKant210 <kevinkant@knights.ucf.edu>
2023-04-29 11:45:25 -0400

    sync

--- api/weaviate.js
+++ api/weaviate.js
@@ -1,10 +1,18 @@
-import express from 'express';
+
 export const weaviateRouter = express.Router();
 import weaviate from 'weaviate-ts-client';
+import  * as dotenv from 'dotenv';
+dotenv.config();
+
+var envVariables = process.env;
+const {
+    OPENAIKEY
+} = envVariables;
 
 const client = weaviate.client({
     scheme: 'http',
     host: 'localhost:8080',  // Replace with your endpoint
+    headers: {'X-OpenAI-Api-Key': OPENAIKEY},  // Replace with your inference API key
 });
 
 const schema = await client.schema.getter().do();
