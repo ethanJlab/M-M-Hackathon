8a66be91c1c18ff956b2d770e5ba6fe69ec75bc3
KevinKant210 <kevinkant@knights.ucf.edu>
2023-04-29 13:00:01 -0400

    For ethan Co-authored-by: Ethan Janicki <ethanJlab@users.noreply.github.com>

--- api/weaviate.js
+++ api/weaviate.js
@@ -1,5 +1,5 @@
 
-export const weaviateRouter = express.Router();
+
 import weaviate from 'weaviate-ts-client';
 import  * as dotenv from 'dotenv';
 dotenv.config();
@@ -14,6 +14,40 @@ const client = weaviate.client({
     host: 'localhost:8080',  // Replace with your endpoint
     headers: {'X-OpenAI-Api-Key': OPENAIKEY},  // Replace with your inference API key
 });
+
+const schemaConfig = {
+    'class': 'Document',
+    'vectorizer': 'text2vec-openai',
+    'vectorIndexType': 'hnsw',
+    'moduleConfig': {
+        "text2vec-openai": {
+            "model": "ada",
+            "modelVersion": "002",
+            "type": "text"
+          }
+    },
+    'properties': [
+        {
+            "dataType": [
+              "text"
+            ],
+            "description": "Content that will be vectorized",
+            "moduleConfig": {
+              "text2vec-openai": {
+                "skip": false,
+                "vectorizePropertyName": false
+              }
+            },
+            "name": "content"
+        }
+    ]
+}
+
+await client.schema
+    .classCreator()
+    .withClass(schemaConfig)
+    .do();
+
 
 const schema = await client.schema.getter().do();
 
