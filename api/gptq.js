29d6c5d3457dfa0992974eee0521c29ba7f8bc26
KevinKant210 <kevinkant@knights.ucf.edu>
2023-04-29 15:02:40 -0400

    Its ALIVE

---
+++ api/gptq.js
@@ -0,0 +1,44 @@
+import fs from "fs";
+import weaviate from 'weaviate-ts-client';
+import  * as dotenv from 'dotenv';
+dotenv.config();
+
+var envVariables = process.env;
+const {
+    OPENAIKEY
+} = envVariables;
+
+const client = weaviate.client({
+    scheme: 'http',
+    host: 'localhost:8080',  // Replace with your endpoint
+    headers: {'X-OpenAI-Api-Key': OPENAIKEY},  // Replace with your inference API key
+});
+
+
+const generateTask = 'What is the document concerning Donvan';
+
+var keywords = "Accident, Donvan, Date, Incident, Crash, Injury, Report, Police, Statement, Witness";
+
+client.graphql
+  .get()
+  .withClassName('Document')
+  .withFields('content')
+  .withNearText({
+    concepts: [keywords]
+  })
+  .withGenerate({
+    groupedTask: generateTask,
+  })
+  .withLimit(1)
+  .do()
+  .then(res => {
+    // console.log(JSON.stringify(res))
+    console.log(res.data.Get.Document[0]._additional.generate.groupedResult)
+    
+  })
+  .catch(err => {
+    console.error(err)
+  });
+
+
+        
