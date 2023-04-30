df1b2657f68b69dbd30014f8b3e9ae59f3f857d1
sebsours <sebsours@gmail.com>
2023-04-29 22:28:46 -0400

    improved UI for chat bubbles

--- better-call-gpt/src/components/Chat.jsx
+++ better-call-gpt/src/components/Chat.jsx
@@ -3,20 +3,18 @@ import { Button, TextField } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import Grid from '@mui/material/Grid';
 
-export function Chat()
-{
+export function Chat() {
     const [text, setText] = useState("");
     const [sendTextBubbles, setSendTextBubble] = useState([]);
     const [recieveTextBubbles, setReceiveTextBubble] = useState([]);
 
     const handleTextSubmit = (e) => {
         e.preventDefault();
-        if (text) 
-        {
+        if (text) {
             setSendTextBubble([...sendTextBubbles, text]);
             console.log(sendTextBubbles);
             setText("");
-            
+
         }
     };
 
@@ -24,32 +22,30 @@ export function Chat()
         let tempBubbles = [];
 
         sendTextBubbles.map((textBubble, index) => {
-            if (index % 2 == 0)
-            {
+            if (index % 2 === 0) {
                 tempBubbles.push(
-                    <div>
-                        <div key={index} className='w-1/2 h-10 rounded bg-violet-500 float-right'>
-                            <span>{textBubble}</span>
+                    <div className='grid grid-cols-2 content-end my-7 mx-4'>
+                        <div key={index} className='w-full rounded bg-yellow-400 col-end-3 px-3 py-2 mx-2'>
+                            <span className='font-mono'>{textBubble}</span>
                         </div>
-                        <div className='w-1/2 h-10 rounded transparent float-left'>
+                        <div className='w-full rounded transparent '>
                         </div>
                     </div>
-                    
+
                 )
             }
-            else
-            {
+            else {
                 tempBubbles.push(
-                    <div>
-                        <div key={index} className='w-1/2 h-10 rounded bg-violet-500 float-left'>
-                            <span>{textBubble}</span>
+                    <div className='grid grid-cols-2 my-7 mx-2'>
+                        <div key={index} className='w-full rounded bg-yellow-400 col-start-1 px-3 py-2 mx-2'>
+                            <span className='font-mono'>{textBubble}</span>
                         </div>
-                        <div className='w-1/2 h-10 rounded transparent float-right'>
+                        <div className='w-full rounded transparent float-right'>
                         </div>
                     </div>
                 )
             }
-            
+
         })
 
         setReceiveTextBubble(tempBubbles);
@@ -58,43 +54,43 @@ export function Chat()
     return (
         // <div className='w-full h-96 grid grid-rows-2 content-end'>
         <Grid
-        container
-        direction="column"
-        justifyContent="flex-end"
-        alignItems="center"
-        style={{ position: 'fixed', bottom: '0', width: '75%', paddingRight:"8px", paddingLeft:"8px", paddingBottom:"8px" }}
+            container
+            direction="column"
+            justifyContent="flex-end"
+            alignItems="center"
+            style={{ position: 'fixed', bottom: '0', width: '75%', paddingRight: "8px", paddingLeft: "8px", paddingBottom: "8px" }}
         >
-            <Grid item xl={12} lg={12} md={12} sm={12} xs={12} style={{width:"100%",  maxHeight:"83vh", overflowY:"scroll"}}>
+            <Grid item xl={12} lg={12} md={12} sm={12} xs={12} style={{ width: "100%", maxHeight: "83vh", overflowY: "scroll" }}>
                 {recieveTextBubbles}
             </Grid>
 
             {/* <div className='flex justify-center content-center'>
             //     <div className="w-11/12 h-12 rounded-lg bg-white"> */}
-            <Grid item xl={12} lg={12} md={12} sm={12} xs={12} style={{width:"100%"}}>
+            <Grid item xl={12} lg={12} md={12} sm={12} xs={12} style={{ width: "100%" }}>
 
-          
-            <TextField placeholder="Ask a question about the case!" value={text} className='w-full' onChange={(e) => setText(e.target.value)}
-                onKeyDown={(event) => {
-                    if (event.key === "Enter") {
-                    handleTextSubmit(event);
-                    }
-                }}
-                
-                InputProps={{
-                    endAdornment: (
-                        <Button onClick={handleTextSubmit}>
-                            <SendIcon/>
-                        </Button>
-                    ),
-                }}>
-                
-            </TextField>
+
+                <TextField placeholder="Ask a question about the case!" value={text} className='w-full' onChange={(e) => setText(e.target.value)}
+                    onKeyDown={(event) => {
+                        if (event.key === "Enter") {
+                            handleTextSubmit(event);
+                        }
+                    }}
+
+                    InputProps={{
+                        endAdornment: (
+                            <Button onClick={handleTextSubmit}>
+                                <SendIcon />
+                            </Button>
+                        ),
+                    }}>
+
+                </TextField>
             </Grid>
-                
-        {/* //         </div> */}
-        {/* //     </div> */}
-            
-        {/* // </div> */}
+
+            {/* //         </div> */}
+            {/* //     </div> */}
+
+            {/* // </div> */}
         </Grid>
     );
 }
